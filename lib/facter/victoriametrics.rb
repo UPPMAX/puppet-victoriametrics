2419232c3c3ed9bb66b7c937661340b95f3251f0
Pär Lindfors <par.lindfors@naiss.se>
2026-03-14 02:18:59 +0100

    Add vmutils version facts

--- lib/facter/victoriametrics.rb
+++ lib/facter/victoriametrics.rb
@@ -4,11 +4,20 @@ if File.exist? "/opt/victoriametrics/bin" then
   Facter.add(:victoriametrics) do
     setcode do
       victoriametrics = {}
-      # Get versions for all three victoriametrics cluster binaries
+      # Get versions for all victoriametrics binaries
       [
+        # cluster tarball:
         "vminsert",
         "vmselect",
         "vmstorage",
+        # vmutils tarball:
+        "vmagent",
+        "vmalert",
+        "vmalert-tool",
+        "vmauth",
+        "vmbackup",
+        "vmctl",
+        "vmrestore",
       ].each do |binary|
         version = Facter::Core::Execution.execute("/opt/victoriametrics/bin/#{binary}-prod -version", { on_fail: nil })
         victoriametrics[:"#{binary}_version"] = version.match(%r{v\d+\.\d+\.\d+}).to_s if version
